80de094ef82d88f52102402a2314001a19e85845
Trikyname <alvarocolomvidal@gmail.com>
2021-04-03 13:49:33 +0200

    Anims

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speedMove=2;
+    [SerializeField] private float _speedMove = 2;
     [SerializeField] private Animator _animator;
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private BoxCollider2D _boxCollider2D;
-
+    public bool isIdle;
     private void Awake()
     {
-        _animator=transform.GetComponent<Animator>();
-        _rigidbody2D = transform.GetComponent<Rigidbody2D>();
-        _boxCollider2D = transform.GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (_rigidbody2D.velocity.x < 0.9 && _rigidbody2D.velocity.x > -0.9)
+            isIdle = true;
+        else
+            isIdle = false;
+
+        Debug.Log(_rigidbody2D.velocity);
+        _animator.SetFloat("Speed", _rigidbody2D.velocity.x);
+        _animator.SetBool("Idle", isIdle);
+
     }
     private void FixedUpdate()
     {
@@ -50,6 +60,17 @@ public class PlayerMovement : MonoBehaviour
             //_rigidbody2D.AddForce(new Vector2(1, 0) * Time.deltaTime * _speedMove);
             _rigidbody2D.velocity = new Vector2(0, 1) * _speedMove * 2;
         }
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("X");
+            _animator.SetTrigger("Attack");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Y");
+
+            _animator.SetTrigger("Throw");
+        }
+
     }
 }
